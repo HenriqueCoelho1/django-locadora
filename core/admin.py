6d9b92e35bd417d65e7714ea74af51189185da9d
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2022-01-18 20:17:00 -0300

    models and upload image

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+from .models import Position, Service, Employee
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("position", "is_active", "modified")
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("service", "icon", "is_active", "modified")
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("name", "position", "is_active", "modified")

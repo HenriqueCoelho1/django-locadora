13777fb75443926e1bbcd5bb255b44c82d243af1
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2022-01-20 19:47:27 -0300

    sending email

--- core/views.py
+++ core/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.views.generic import FormView
 
 from .models import Service, Employee, Position, Feature
-class IndexView(TemplateView):
+from .forms import ContactForm
+class IndexView(FormView):
     template_name = "index.html"
+    form_class = ContactForm
+    success_url = reverse_lazy("index")
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
@@ -12,3 +17,13 @@ class IndexView(TemplateView):
         context["features_one"] = Feature.objects.all()[:3]
         context["features_two"] = Feature.objects.all()[3:6]
         return context
+    
+    def form_valid(self, form, *args ,**kwargs):
+        form.send_email()
+        messages.success(self.request, "E-mail send with success")
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args ,**kwargs):
+        messages.error(self.request, "Error in send this e-mail")
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
+

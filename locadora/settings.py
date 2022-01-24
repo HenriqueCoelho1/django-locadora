6e390f5e4938a7559db60b92d0fe17ed92dd2a6b
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2022-01-24 20:56:34 -0300

    module users

--- locadora/settings.py
+++ locadora/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-*(d6&ob&2a-g6d(qb-lh#h=c%gm!fa!$@wz1a^x2#qs8ad2_6d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -76,11 +76,11 @@ WSGI_APPLICATION = "locadora.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.config()
-}
+# DATABASES = {
+#     "default": dj_database_url.config()
+# }
 
-"""DATABASES = {
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "django_loc",
@@ -90,7 +90,7 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
---
+++ users/urls.py
@@ -0,0 +1,2 @@
+from django.urls import urls
+from django.contrib.auth import views as auth_views

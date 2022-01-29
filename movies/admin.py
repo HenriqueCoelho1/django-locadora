5e86de5141560d480447f0fa26a51dc15bec0629
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2022-01-28 22:23:20 -0300

    fixing update and register

--- movies/admin.py
+++ movies/admin.py
@@ -4,10 +4,10 @@ from .models import Movie, Genre
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ("name", "description", "price", "stock")
+    list_display = ("name", "description", "price", "stock", "get_genres")
 
-    # def get_genres(self, obj):
-    #     return ", ".join([m.name for m in obj.genres.all()])
+    def get_genres(self, obj):
+        return ", ".join([m.name for m in obj.genre.all()])
 
 
 @admin.register(Genre)
